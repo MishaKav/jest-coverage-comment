b431652daa3a60e268a7e2227ac59e85e09e8c45
Misha Kav <misha.kav@gmail.com>
2022-03-22 01:29:51 +0200

    add coverage to alt prop

--- src/summary.ts
+++ src/summary.ts
@@ -45,7 +45,7 @@ function summaryToMarkdown(summary: Summary, options: Options): string {
   const { statements, functions, branches } = summary
   const { color, coverage } = getCoverage(summary)
   const readmeHref = `https://github.com/${repository}/blob/${commit}/README.md`
-  const badge = `<a href="${readmeHref}"><img alt="${badgeTitle}" src="https://img.shields.io/badge/${badgeTitle}-${coverage}%25-${color}.svg" /></a><br/>`
+  const badge = `<a href="${readmeHref}"><img alt="${badgeTitle}: ${coverage}%" src="https://img.shields.io/badge/${badgeTitle}-${coverage}%25-${color}.svg" /></a><br/>`
 
   const table = `| Lines | Statements | Branches | Functions |
 | ----- | ------- | -------- | -------- |
