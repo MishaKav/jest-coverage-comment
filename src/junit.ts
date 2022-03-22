1bd86fadf9a272b57b87ba2c4e84deabe46f197d
Misha Kav <misha.kav@gmail.com>
2022-03-22 02:13:28 +0200

    fix junit exports

--- src/junit.ts
+++ src/junit.ts
@@ -4,44 +4,6 @@ import * as xml2js from 'xml2js'
 import { Junit, JunitReport, Options } from './types.d'
 import { getContentFile } from './utils'
 
-// return junit report
-export async function getJunitReport(options: Options): Promise<JunitReport> {
-  const { junitFile } = options
-
-  try {
-    if (junitFile) {
-      const xmlContent = getContentFile(junitFile)
-      const parsedXml = await parseJunit(xmlContent)
-
-      if (parsedXml) {
-        const junitHtml = junitToMarkdown(parsedXml, options)
-
-        return {
-          junitHtml,
-          tests: 0,
-          skipped: 0,
-          failures: 0,
-          errors: 0,
-          time: 0,
-        }
-      }
-    }
-  } catch (error) {
-    if (error instanceof Error) {
-      core.error(`Error on generating junit report. ${error.message}`)
-    }
-  }
-
-  return {
-    junitHtml: '',
-    tests: 0,
-    skipped: 0,
-    failures: 0,
-    errors: 0,
-    time: 0,
-  }
-}
-
 // parse junit.xml to Junit object
 async function parseJunit(xmlContent: string): Promise<Junit | null> {
   try {
@@ -96,4 +58,43 @@ ${table}`
   }
 
   return table
+}
+
+// return junit report
+export async function getJunitReport(options: Options): Promise<JunitReport> {
+  const { junitFile } = options
+
+  try {
+    if (junitFile) {
+      const xmlContent = getContentFile(junitFile)
+      const parsedXml = await parseJunit(xmlContent)
+
+      if (parsedXml) {
+        const junitHtml = junitToMarkdown(parsedXml, options)
+        const { skipped, errors, failures, tests, time } = parsedXml
+
+        return {
+          junitHtml,
+          tests,
+          skipped,
+          failures,
+          errors,
+          time,
+        }
+      }
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      core.error(`Error on generating junit report. ${error.message}`)
+    }
+  }
+
+  return {
+    junitHtml: '',
+    tests: 0,
+    skipped: 0,
+    failures: 0,
+    errors: 0,
+    time: 0,
+  }
 }
